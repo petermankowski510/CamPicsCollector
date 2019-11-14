2c8cfe5197e46823854a379041c87911ff25be3b
PeterMankowskiNeuro <47701894+PeterMankowskiNeuro@users.noreply.github.com>
2019-11-14 18:28:16 -0500

    Added resizing

--- CamSnap.py
+++ CamSnap.py
@@ -13,6 +13,8 @@ import cv2
 from imutils import paths
 import argparse
 import os 
+import numpy as np
+import matplotlib.pyplot as plt
 
 def variance_of_laplacian(image):
     # compute the Laplacian of the image and then return the focus
@@ -92,5 +94,28 @@ path, dirs, files = next(os.walk("C:\\Users\\seeho\\CamPics"))
 pic_count = len(files)
 print("Number of good pictures:", pic_count)
 print("Deleted pictures:",cnt)
+
+# Lessons in resizing
+# ...................
+image = cv2.imread("C:\\Users\\seeho\\CamPics\\Peter_197.png", 1) 
+# Loading the image 
+  
+half = cv2.resize(image, (0, 0), fx = 0.1, fy = 0.1) 
+bigger = cv2.resize(image, (1050, 1610)) 
+  
+stretch_near = cv2.resize(image, (780, 540),  
+               interpolation = cv2.INTER_NEAREST) 
+  
+  
+Titles =["Original", "Half", "Bigger", "Interpolation Nearest"] 
+images =[image, half, bigger, stretch_near] 
+count = 4
+  
+for i in range(count): 
+    plt.subplot(2, 2, i + 1) 
+    plt.title(Titles[i]) 
+    plt.imshow(images[i]) 
+  
+plt.show() 
 
 
